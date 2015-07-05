60b1afe1f808ae34d40f1dd689744e3a52972100
Cristiano Lopes <e.mail@cristianolopes.eu>
2015-07-05 17:39:47 +0100

    Backport multi apns settings

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,3 +1,14 @@
 from test_models import *
 from test_gcm_push_payload import *
 from test_apns_push_payload import *
+from test_apns_certificates_settings import *
+from test_management_commands import *
+
+
+# conditionally test rest_framework api if the DRF package is installed
+try:
+	import rest_framework
+except ImportError:
+	pass
+else:
+	from test_rest_framework import *
